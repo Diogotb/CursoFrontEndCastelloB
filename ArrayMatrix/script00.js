884bd7b2729a657a476a96ba8ed20d3c6d357ecd
Diogotb <digogo@gmail.com>
2025-02-25 11:06:27 -0300

    25.02.25

--- ArrayMatrix/script00.js
+++ ArrayMatrix/script00.js
@@ -32,7 +32,44 @@ arrayTexto.pop();
 console.log(arrayTexto);
 
 //percorrer um Array (laço de repetição) - 
-//percorrer o array de números
+//percorrer o array de números (for)
 for(let i =0;i<arrayNumeros.length;i++){
     console.log("índice["+i+"]="+[arrayNumeros[i]])
 }
+
+//forEach 
+arrayTexto.forEach(elemento => {
+    console.log(elemento)
+});
+
+// Métodos úteis
+
+// IndexOf
+console.log(arrayNumeros.indexOf(5)); //4
+console.log(arrayNumeros.indexOf(10)); //-1 (elementos inexistente)
+
+//Splice (remover elementos da posição)
+arrayMisto.splice(2,1);
+console.log(arrayMisto);
+
+//Operações avançadas de Arrays
+// map - novos valores 
+let valores = [10, 20, 30, 40, 50];
+let valoresDobro = valores.map(x => x*2);
+console.log(valoresDobro);
+//filter - comparação
+let valoresFilter = valores.filter(x => x>25);
+console.log(valoresFilter);
+
+//desafio (filtro x<35) && (x*3) = [30, 60, 90]
+let valoresDesafio = valores.filter(x=>x<35).map(x=>x*3);
+
+//reduce ([] -> let x)
+let soma = valores.reduce((ValorSoma,ValorElemento) => ValorSoma+ValorElemento);
+console.log(soma);
+
+//sort (organizar/ordenar)
+let aleatorio = [7, 4, 2, 9, 1, 5, 8, 3, 6];
+console.log(aleatorio);
+let ordenado = aleatorio.sort();
+console.log(ordenado);
