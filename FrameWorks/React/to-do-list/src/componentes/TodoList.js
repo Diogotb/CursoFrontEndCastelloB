e3cd02dc71cc71f5bda7bab0f55bdf1bf2425b6c
Diogotb <digogo@gmail.com>
2025-09-02 16:57:52 -0300

    ok

--- FrameWorks/React/to-do-list/src/componentes/TodoList.js
+++ FrameWorks/React/to-do-list/src/componentes/TodoList.js
@@ -1,19 +1,16 @@
-import "./TodoList.css";
+import "./ToDoList.css";
 
-const TodoList = ({tasks, removeTask}) =>{
-    //retorna a lista de tarefas
+const ToDoList = ({ tarefas , removerTarefa}) =>{
     return(
         <ul>
-            {/* faz o mapemaneto do vetor de tarfeas e cria um item para cada tarefa da lista */}
-            {tasks.map((task, index) => (
+            {tarefas.map((tarefa,index) =>(
                 <li key={index}>
-                    {task} <button onClick={() => removeTask(index)}>
-                        Excluir
-                    </button>
+                    {tarefa}
+                    <button onClick={()=>removerTarefa(index)}>Excluir</button>
                 </li>
             ))}
         </ul>
     );
 };
 
-export default TodoList;
+export default ToDoList;
